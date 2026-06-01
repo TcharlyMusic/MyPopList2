a81ec2c018a3e1e607361a380bc21e88dafae9cf
TcharlyMusic <duprezjc@gmail.com>
2026-06-01 17:38:00 +0200

    Update Datas and Version

--- src/main.js
+++ src/main.js
@@ -620,7 +620,7 @@ async function CreateTableResults(selectGroupValue, selectLicenseValue, selectZo
 	document.getElementById("selectGroupALL").text = 'Toutes les Pops (' + numberOfPopALL + ')';
 
 	// Update the Number of Pops Found and the Version
-	document.getElementById("versionOfPops").innerHTML = 'Pops found = ' + numberOfPopFound + ' / Version 0.9.86 (2026-05-20)';
+	document.getElementById("versionOfPops").innerHTML = 'Pops found = ' + numberOfPopFound + ' / Version 0.9.87 (2026-06-01)';
 
 	// Console Estimation Infos
 	console.log('Estimation Totale = ' + totalEstimation + ' Euros');
